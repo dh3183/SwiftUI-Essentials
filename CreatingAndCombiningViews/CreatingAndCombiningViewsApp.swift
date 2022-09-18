c8b2cc022a9a463174cf5a976078686aafe292ca
Elbin <83414134+dh3183@users.noreply.github.com>
2022-09-18 18:18:53 +0900

    Initial Commit

---
+++ CreatingAndCombiningViews/CreatingAndCombiningViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAndCombiningViewsApp.swift
+//  CreatingAndCombiningViews
+//
+//  Created by 한빈 on 2022/09/18.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAndCombiningViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
